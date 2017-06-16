b94eaaab52e3bb212d146f82d1370e3536ffb454
Gavin Mogan <git@gavinmogan.com>
2017-06-15 20:15:54 -0700

    cleanup

--- flask_atlassian_connect/__init__.py
+++ flask_atlassian_connect/__init__.py
@@ -1,13 +1,7 @@
-import logging
-
-logger = logging.getLogger('flask_atlassian_connect')
-logger.addHandler(logging.StreamHandler())
-if logger.level == logging.NOTSET:
-    logger.setLevel(logging.WARN)
-
-__version__ = '0.0.2'
-__url__ = 'https://github.com/halkeye/ac-flask'
+__version__ = '0.0.1'
+__url__ = 'https://github.com/halkeye/flask_atlassian_connect'
 __author__ = 'Gavin Mogan'
 __email__ = 'opensource@gavinmogan.com'
+__all__ = ['base']
 
 from .base import AtlassianConnect  # NOQA: E402, F401, C0413
