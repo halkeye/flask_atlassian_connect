838fe79eb1a76033b9c4eba25b41fcd25f35b4d9
Gavin Mogan <git@gavinmogan.com>
2017-06-26 14:24:50 -0700

    python3 fix for __all__ not containing classes

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -2,6 +2,7 @@
 ------------------
 
 - Migrate away from requireing BASE_URL to be set and just use flask's own url handling [halkeye]
+- fix for __all__ to actually return the classes not the libraries (still learning about python3) [halkeye]
 
 
 0.0.1 (2017-06-21)
--- flask_atlassian_connect/__init__.py
+++ flask_atlassian_connect/__init__.py
@@ -2,7 +2,7 @@ __version__ = '0.0.2.dev0'
 __url__ = 'https://github.com/halkeye/flask_atlassian_connect'
 __author__ = 'Gavin Mogan'
 __email__ = 'opensource@gavinmogan.com'
-__all__ = ['base', 'default_client']
+__all__ = ['AtlassianConnect', 'AtlassianConnectClient']
 
 from .base import AtlassianConnect  # NOQA: E402, F401, C0413
 from .client import AtlassianConnectClient  # NOQA: E402, F401, C0413
